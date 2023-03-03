cac81aee005a4bb74f945cf0dcea0b133f99da22
fortdev210 <sharp.blader2050@gmail.com>
2023-03-04 04:14:40 +0800

    update validate env

--- src/utils/validate-env.ts
+++ src/utils/validate-env.ts
@@ -2,10 +2,6 @@ import { cleanEnv, port, str, email } from "envalid";
 
 const validateEnv = () => {
   cleanEnv(process.env, {
-    DB_HOST: str(),
-    DB_PORT: port(),
-    POSTGRES_USER: str(),
-    POSTGRES_PASSWORD: str(),
     POSTGRES_DB: str(),
   });
 };
